34d8abd1199ae2da085f03f7b238ce4c6d72d278
Julian Huiskamp <julian.huiskamp@kadaster.nl>
2023-11-16 15:04:50 +0100

    teruggezet naar einde vorige branch

--- src/main/java/guru/springframework/spring6restmvc/config/SpringSecConfig.java
+++ src/main/java/guru/springframework/spring6restmvc/config/SpringSecConfig.java
@@ -13,10 +13,7 @@ public class SpringSecConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
-                .anyRequest().authenticated()
-                .and().oauth2ResourceServer().jwt();
-
+        http.csrf().ignoringRequestMatchers("/api/**");
         return http.build();
     }
 
